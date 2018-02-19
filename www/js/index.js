9fc6061318a86f08908c03a55645cf1bdf0ac98b
gymvideo2 <36563733+gymvideo2@users.noreply.github.com>
2018-02-19 17:54:58 +0100

    s

--- www/js/index.js
+++ www/js/index.js
@@ -244,18 +244,14 @@ function displayContents(err, text){
 		}
 		
 		function animazione(){
-			var options = {
-  "direction"        : "up", // 'left|right|up|down', default 'left' (which is like 'next')
-  "duration"         :  500, // in milliseconds (ms), default 400
-  "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1). -1 doesn't slide at all. Default 4
-  "slidePixels"      :   20, // optional, works nice with slowdownfactor -1 to create a 'material design'-like effect. Default not set so it slides the entire page.
-  "iosdelay"         :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
-  "androiddelay"     :  150, // same as above but for Android, default 70
-  "winphonedelay"    :  250, // same as above but for Windows Phone, default 200,
-  "fixedPixelsTop"   :    0, // the number of pixels of your fixed header, default 0 (iOS and Android)
-  "fixedPixelsBottom":   60  // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
+var options = {
+  "direction"      : "up", // 'left|right|up|down', default 'right' (Android currently only supports left and right)
+  "duration"       :  600, // in milliseconds (ms), default 400
+  "iosdelay"       :   50, // ms to wait for the iOS webview to update before animation kicks in, default 60
+  "androiddelay"   :  100,  // same as above but for Android, default 70
+  "winphonedelay"  :  150 // same as above but for Windows Phone, default 200
 };
-window.plugins.nativepagetransitions.slide(
+window.plugins.nativepagetransitions.flip(
   options,
   function (msg) {console.log("success: " + msg)}, // called when the animation has finished
   function (msg) {alert("error: " + msg)} // called in case you pass in weird values
