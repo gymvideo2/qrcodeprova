5e955ce06372d00e35841b53c3843800a7b5dea6
gymvideo2 <36563733+gymvideo2@users.noreply.github.com>
2018-03-03 20:16:08 +0100

    v

--- www/assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js
+++ www/assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js
@@ -71,9 +71,9 @@
 
         var dt = (e.timeStamp - this.startTime) / 1000
         var speed = Math.abs(this.dx / dt) // percent-per-second
-        if (this.dx > 80 || (this.dx > 0 && speed > this.options.swipe)) {
+        if (this.dx > 90 || (this.dx > 0 && speed > this.options.swipe)) {
             this.carousel.prev()
-        } else if (this.dx < -80 || (this.dx < 0 && speed > this.options.swipe)) {
+        } else if (this.dx < -90 || (this.dx < 0 && speed > this.options.swipe)) {
             this.carousel.next();
         } else {
             this.$active
