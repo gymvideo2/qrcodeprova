594a2a02fb344674fd6def58d85ae795e79aa207
gymvideo2 <g.tassinari@outlook.com>
2019-01-25 17:28:47 +0100

    daje con sta localizzazione
    se mi leggi, ciao LEO!

--- www/js/index.js
+++ www/js/index.js
@@ -67,6 +67,7 @@ var app = {
 				//se la pagina è lo splash screen carica tutt
 				checkLanguage();
 				carica_sistema_notifiche();
+				localizzazione_menu();
 				window.QRScanner.prepare(onDone); // show the prompt
 				getWelcome();
 		}
@@ -86,7 +87,6 @@ var app = {
 				
 				if (sPage == "contatti.html"){
 					
-					localizzazione_menu();
 
 				}
 				
